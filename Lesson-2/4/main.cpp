d151b1f43f5a5fa1fd2af85954be8c7c414ed5b8
sh-alanova <38557996+sh-alanova@users.noreply.github.com>
2018-04-27 21:33:12 +0600

    Перерешала задание 3

--- Lesson-2/4/main.cpp
+++ Lesson-2/4/main.cpp
@@ -3,7 +3,12 @@
 using namespace std;
 int main()
 {
-    int a, b;
-    scanf("%d %d", &a, &b);
-    printf("%d", a + b);
+    int a, b, n;
+    scanf("%d%d", a, b);
+    scanf("%d", n);
+    int sum = a * 100 + b;
+    int price = sum * n;
+    int rub = price / 100;
+    int kop = price % 100;
+    printf("%d %d", rub, kop);
 }
