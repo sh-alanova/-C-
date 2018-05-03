8c57ff3e6354211641afd90b23462e7a7a1d9932
sh-alanova <38557996+sh-alanova@users.noreply.github.com>
2018-05-03 19:29:22 +0600

    Обновила задание 4

--- Lesson-2/4/main.cpp
+++ Lesson-2/4/main.cpp
@@ -1,14 +1,13 @@
-#include <iostream>
 #include <stdio.h>
-using namespace std;
-int main()
-{
+
+int main(){
     int a, b, n;
-    scanf("%d%d", a, b);
-    scanf("%d", n);
+    scanf("%d%d", &a, &b);
+    scanf("%d", &n);
     int sum = a * 100 + b;
     int price = sum * n;
     int rub = price / 100;
     int kop = price % 100;
     printf("%d %d", rub, kop);
 }
+
