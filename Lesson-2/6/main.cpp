8b644268138d0167613dc3d9511e68a0134c1b7b
sh-alanova <38557996+sh-alanova@users.noreply.github.com>
2018-05-09 14:38:30 +0600

    Исправила все ошибки оформления

--- Lesson-2/6/main.cpp
+++ Lesson-2/6/main.cpp
@@ -1,14 +1,13 @@
 #include <stdio.h>
 
-int main(){
+int main() {
     int n;
     scanf("%d", &n);
     int time = 45 * n;
-    int  even = ((n-1) / 2) * 15;
-    int ood = (((n-1) / 2) + ((n-1) % 2)) * 5;
-    time += even + ood;
+    int even = ((n-1) / 2) * 15;
+    int odd = (((n-1) / 2) + ((n-1) % 2)) * 5;
+    time += even + odd;
     int hour = 9 + time / 60;
     int minute = time % 60;
     printf("%d %d", hour, minute);
 }
-
