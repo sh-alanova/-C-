180b30d943c155e2201879c25d5f9cafeff877cd
sh-alanova <38557996+sh-alanova@users.noreply.github.com>
2018-06-01 22:16:32 +0600

    Исправила ошибки в оформлении

--- Lesson-3/1/main.cpp
+++ Lesson-3/1/main.cpp
@@ -3,8 +3,8 @@
 int main () {
     int n;
     scanf("%d", &n);
-    if ((n % 4 == 0 && n % 100 !=0) || n % 400 == 0) {
-        printf( "YES");
+    if ((n % 4 == 0 && n % 100 != 0) || n % 400 == 0) {
+        printf("YES");
     }
-    else printf( "NO");
+    else printf("NO");
 }
