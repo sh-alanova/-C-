8087af707fa0fb41f68d09074b056bbdd1807090
sh-alanova <38557996+sh-alanova@users.noreply.github.com>
2018-06-24 18:29:04 +0600

    Изменила код задания 2 занятия 3

--- Lesson-3/2/main.cpp
+++ Lesson-3/2/main.cpp
@@ -1,16 +1,15 @@
 #include <stdio.h>
 
 int main() {
-    int a, b, c;
-    scanf("%d %d %d", &a, &b, &c);
-    if(a >= b && a >= c) {
-        printf("%d", a);
+    int a, b, c, max;
+    scanf("%d%d%d", &a, &b, &c);
+    if(a > b) {
+        max = a;
+    } else {
+        max = b;
     }
-    else if(b >= a && b >= c) {
-        printf("%d", b);
+    if(c > max) {
+        max = c;
     }
-    else if(c >= b && c >= a) {
-        printf("%d", c);
-    }
+    printf("%d", max);
 }
-
