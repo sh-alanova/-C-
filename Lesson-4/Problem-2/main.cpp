7abc8f718c17d36872cdbb22857870796239ca4d
sh-alanova <38557996+sh-alanova@users.noreply.github.com>
2018-06-24 18:39:38 +0600

    Переименовала переменные

--- Lesson-4/Problem-2/main.cpp
+++ Lesson-4/Problem-2/main.cpp
@@ -3,14 +3,14 @@
 int main () {
     int n;
     scanf("%d", &n);
-    int arrey[n], zero = 0;
+    int array[n], count = 0;
     for (int i = 0; i < n; ++i) {
-        scanf("%d", &arrey[i]);
+        scanf("%d", &array[i]);
     }
     for (int i = 0; i < n; ++i) {
-        if (arrey[i] == 0) {
-            ++zero;
+        if (array[i] == 0) {
+            ++count;
         }
     }
-    printf("%d", zero);
+    printf("%d", count);
 }
