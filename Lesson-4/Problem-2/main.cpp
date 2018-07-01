6a019d32968645250d3380c053c606e86b1536f0
sh-alanova <38557996+sh-alanova@users.noreply.github.com>
2018-07-01 13:17:39 +0600

    Обновила задание 2 занятия 4

--- Lesson-4/Problem-2/main.cpp
+++ Lesson-4/Problem-2/main.cpp
@@ -3,10 +3,11 @@
 int main () {
     int n;
     scanf("%d", &n);
-    int array[n], count = 0;
+    int array[n];
     for (int i = 0; i < n; ++i) {
         scanf("%d", &array[i]);
     }
+    int count = 0;
     for (int i = 0; i < n; ++i) {
         if (array[i] == 0) {
             ++count;
