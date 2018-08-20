f80ed9f0dd7814dc2dba8e8cce00f897dd110418
sh-alanova <38557996+sh-alanova@users.noreply.github.com>
2018-08-20 10:59:48 +0600

    Update main.cpp

--- Lesson-6/Problem-1/main.cpp
+++ Lesson-6/Problem-1/main.cpp
@@ -4,7 +4,9 @@ int main() {
     int h, m;
     char x;
     scanf("%d:%d %c", &h, &m, &x);
-    if(x == 'p' && h != 12) {
+    if(h == 12 && x == 'a'){
+        printf("00:%d", m);
+    } else if(x == 'p' && h != 12) {
         h += 12;
         printf("%02d:%02d", h, m);
     } else {
