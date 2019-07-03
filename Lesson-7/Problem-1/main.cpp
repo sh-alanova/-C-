c3fe9e191c49f8f620ae6afedc7da5a256942e9a
sh-alanova <38557996+sh-alanova@users.noreply.github.com>
2019-07-03 15:34:11 +0600

    Код задания 1 занятия 7

---
+++ Lesson-7/Problem-1/main.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    FILE * fin = fopen("input.txt", "r");
+    FILE * fout = fopen("output.txt", "w");
+    int number, sum = 0;
+    while(fscanf(fin, "%d", &number) == 1) {
+        fprintf(fout, "%d\n", number);
+        sum += number;
+    }
+    fprintf(fout, "%d\n", sum);
+    fclose(fin);
+    fclose(fout);
+}
