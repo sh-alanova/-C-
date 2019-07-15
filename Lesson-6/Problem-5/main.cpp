9220c758354aeb61f7273f7f33b214531e4a2781
sh-alanova <38557996+sh-alanova@users.noreply.github.com>
2019-07-15 23:22:11 +0600

    Изменила код

--- Lesson-6/Problem-5/main.cpp
+++ Lesson-6/Problem-5/main.cpp
@@ -3,9 +3,14 @@
 
 int main() {
     int a, b, c, d;
-    scanf("%d.%d.%d.%d", &a, &b, &c, &d);
-    if((0 <= a && a <= 255) && (0 <= b && b <= 255) && (0 <= c && c <= 255) && (0 <= d && d <= 255)) {
-        printf("YES");
+    char str;
+    if(scanf("%d.%d.%d.%d %c", &a, &b, &c, &d, &str) == 4) {
+        if((0 <= a && a <= 255)
+           && (0 <= b && b <= 255)
+           && (0 <= c && c <= 255)
+           && (0 <= d && d <= 255)) {
+            printf("YES");
+        }
     } else {
         printf("NO");
     }
