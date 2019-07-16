bf88d87d3b09df5b6a9c3857656718f07fd6c9ac
sh-alanova <38557996+sh-alanova@users.noreply.github.com>
2019-07-16 22:39:34 +0600

    Обновила код

--- Lesson-7/Problem-3/main.cpp
+++ Lesson-7/Problem-3/main.cpp
@@ -2,9 +2,11 @@
 #include <stdlib.h>
 
 int main() {
-    FILE * fin = fopen("input.txt", "rb");
+    FILE *fin = fopen("input.dat", "rb");
+    FILE *fout = fopen("output.txt", "w");
     fseek(fin, 0, SEEK_END);
-    int file_size = ftell(fin);
-    printf("%d", file_size);
-    return 0;
+    int file_size = (int) ftell(fin);
+    fprintf(fout, "%d", file_size);
+    fclose(fin);
+    fclose(fout);
 }
