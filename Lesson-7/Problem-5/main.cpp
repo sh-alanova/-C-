83501315c220c019af509241b2c99edab6cd731b
sh-alanova <38557996+sh-alanova@users.noreply.github.com>
2019-07-18 12:17:42 +0600

    Исправила ошибки

--- Lesson-7/Problem-5/main.cpp
+++ Lesson-7/Problem-5/main.cpp
@@ -8,7 +8,7 @@ int main() {
     fscanf(fin, "%s", &str);
     fseek(fin, 0, SEEK_END);
     int file_size = ftell(fin), k;
-    for(int i = file_size; i > 0; --i) {
+    for(int i = file_size - 1; i >= 0; --i) {
         fseek(fin, i, SEEK_SET);
         k = fgetc(fin);
         fputc(k, fout);
