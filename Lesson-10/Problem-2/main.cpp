69f81b24b84cdb2ae0022b9ce0ad72040342e7ff
sh-alanova <38557996+sh-alanova@users.noreply.github.com>
2019-07-19 23:25:51 +0600

    Исправила ошибка

--- Lesson-10/Problem-2/main.cpp
+++ Lesson-10/Problem-2/main.cpp
@@ -5,8 +5,8 @@ int main() {
     int v, t, s;
     scanf("%d%d", &v, &t);
     if(v < 0) {
-        s = 109 - (abs(v) * t % 109);
-    } else if(v > 0) {
+        s = (109 + (v * t % 109)) % 109;
+    } else {
         s = v * t % 109;
     }
     printf("%d", s);
