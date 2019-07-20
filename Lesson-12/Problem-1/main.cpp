8a051241d89ca95d50f372d1e482b552e2b05075
sh-alanova <38557996+sh-alanova@users.noreply.github.com>
2019-07-20 22:39:49 +0600

    Update main.cpp

--- Lesson-12/Problem-1/main.cpp
+++ Lesson-12/Problem-1/main.cpp
@@ -1,21 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 
 int main() {
     int n, k;
     scanf("%d%d", &n, &k);
-    int bit[32];
-    for(int i = 31; i >= 0; --i) {
-        bit[i] = (n >> i) & 1;
-    }
-    for(int i = 0; i < k; ++i) {
-        bit[i] = 0;
-    }
-    int res = 0;
-    for(int i = 31; i >= 0; --i) {
-        res += bit[i] * pow(2, i);
-    }
+    int res = n >> k << k;
     printf("%d\n", res);
     return 0;
 }
