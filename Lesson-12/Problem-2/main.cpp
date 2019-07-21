762dc560520f92eaf6fc995eca7fd0d8a2984ddc
sh-alanova <38557996+sh-alanova@users.noreply.github.com>
2019-07-21 08:12:36 +0600

    Обновила

--- Lesson-12/Problem-2/main.cpp
+++ Lesson-12/Problem-2/main.cpp
@@ -1,14 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 
 int main() {
     int n, k;
     scanf("%d%d", &n, &k);
-    int bit[32];
-    for(int i = 0; i < 32; ++i) {
-        bit[i] = (n >> i) & 1;
-    }
-    printf("%d\n", bit[k]);
+    printf("%d\n", (n >> k) & 1);
     return 0;
 }
