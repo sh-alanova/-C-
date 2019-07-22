a59b835afc4ad8448957d9779a6be2cee524933d
sh-alanova <38557996+sh-alanova@users.noreply.github.com>
2019-07-22 11:29:26 +0600

    Обновила

--- Lesson-11/Problem-5/main.cpp
+++ Lesson-11/Problem-5/main.cpp
@@ -1,25 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void Hanoi(int n, int start, int finish) {
-    if(n == 1) {
-        printf("1 1 2\n");
-        printf("1 2 3\n");
+void move(int n, int i, int g) {
+    int t = 6 - i - g;
+    if (n == 1) {
+        if(g == i % 3 + 1) {
+            printf("%d %d %d\n", n, i, g);
+        }else {
+            printf("%d %d %d\n", n, i, t);
+            printf("%d %d %d\n", n, t, g);
+        }
     } else {
-        int temp = 6 - start - finish;
-        //printf("start = %d, temp = %d, finish = %d\n", start, temp, finish);
-        Hanoi(n - 1, temp, finish);
-        printf("%d %d %d\n", n, start, temp);
-        printf("%d %d %d\n", n - 1, start, temp);
-        printf("%d %d %d\n", n, temp, finish);
-        Hanoi(n-1, temp, finish);
+        if(g == i % 3 + 1) {
+            move(n - 1, i, t);
+            printf("%d %d %d\n", n, i, g);
+            move(n - 1, t, g);
+        } else {
+            move(n - 1, i, g);
+            printf("%d %d %d\n", n, i, t);
+            move(n - 1, g, i);
+            printf("%d %d %d\n", n, t, g);
+            move(n - 1, i, g);
+        }
     }
 }
 
 int main() {
     int n;
     scanf("%d", &n);
-    Hanoi(n, 1, 3);
-
+    move(n, 1, 3);
     return 0;
 }
