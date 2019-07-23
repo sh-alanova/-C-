dbfc1ad39399507699538aa6bdbcad0da3f0d67e
sh-alanova <38557996+sh-alanova@users.noreply.github.com>
2019-07-23 10:58:12 +0600

    Обновила код

--- Lesson-14/Problem-2/main.cpp
+++ Lesson-14/Problem-2/main.cpp
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 
 int main() {
     int n;
     double a, b;
     scanf("%d%lf%lf", &n, &a, &b);
+    double s = (b - a) / RAND_MAX;
     for (int i = 0; i < n; ++i) {
-        printf("%f\n", a + rand() % (b - a + 1));
+        int k = rand();
+        printf("%f\n", a + k * s);
     }
     return 0;
 }
