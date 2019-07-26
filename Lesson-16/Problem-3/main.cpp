0383bb610ff8be961b681a0e1a03c0ac025d6a58
sh-alanova <38557996+sh-alanova@users.noreply.github.com>
2019-07-26 20:18:36 +0600

    Обновила

--- Lesson-16/Problem-3/main.cpp
+++ Lesson-16/Problem-3/main.cpp
@@ -5,8 +5,9 @@ using namespace std;
 int main() {
     int p, x, y;
     cin >> p >> x >> y;
-    int tmp = x * 100 + y;
-    int rub = (tmp + tmp * p / 100) / 100;
-    int kop = (tmp + tmp * p / 100) % 100;
+    int tmp_1 = x * 100 + y;
+    int tmp_2 = tmp_1 + tmp_1 * p / 100;
+    int rub = tmp_2 / 100;
+    int kop = tmp_2 % 100;
     cout << rub << " " << kop << endl;
 }
