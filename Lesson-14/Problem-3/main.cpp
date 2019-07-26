b4fb2449343eabe7bdd09bb0301118fd7372cf91
sh-alanova <38557996+sh-alanova@users.noreply.github.com>
2019-07-26 10:05:34 +0600

    Обновила

--- Lesson-14/Problem-3/main.cpp
+++ Lesson-14/Problem-3/main.cpp
@@ -1,23 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 
 int main() {
     int n;
     long long a, b;
-    scanf("%d%Ld%ld", &n, &a, &b);
+    scanf("%d%lld%lld", &n, &a, &b);
     for (int i = 0; i < n; ++i) {
-        int q = (int)a + rand() % (int)(b - a + 1);
-        int p = (int)a + rand() % (int)(b - a + 1);
-        printf("q = %d\n", q);
-        printf("p = %d\n", p);
-        q = (long long)q;
-        p = (long long)p;
-        printf("Lq = %Ld\n", q);
-        printf("Lp = %Ld\n", p);
-        p << 31;
-        printf("Lp< = %Ld\n", p);
-        printf("%Ld\n", p + q);
+        long long long_rand = (long long)rand() + (((long long)rand()) << 31);
+        long_rand = a + long_rand % (b - a + 1);
+        printf("%lld\n", long_rand);
     }
     return 0;
 }
