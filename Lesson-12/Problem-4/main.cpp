369ca50918c32026db973e0dc51e7cf8e3781fbc
sh-alanova <38557996+sh-alanova@users.noreply.github.com>
2019-07-26 08:39:19 +0600

    Обновила

--- Lesson-12/Problem-4/main.cpp
+++ Lesson-12/Problem-4/main.cpp
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 
 int main() {
     int n, k;
     scanf("%d%d", &n, &k);
-    printf("%d\n", n & ~((int)pow(2, k)));
+    printf("%d\n", n & ~(1 << k));
     return 0;
 }
+
