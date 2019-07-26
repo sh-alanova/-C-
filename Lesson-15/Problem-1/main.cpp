eca00a012c496ce1b674017a18b1877b4a365c4f
sh-alanova <38557996+sh-alanova@users.noreply.github.com>
2019-07-26 20:04:26 +0600

    Код задачи

---
+++ Lesson-15/Problem-1/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    char c;
+    cin >> n >> c;
+    for(int i = 0; i < n; ++i) {
+        cout << c;
+    }
+    return 0;
+}
