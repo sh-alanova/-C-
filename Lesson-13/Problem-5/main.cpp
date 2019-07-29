404cecaaf177137ecc20ab6ab7cef517e7b52b87
sh-alanova <38557996+sh-alanova@users.noreply.github.com>
2019-07-29 08:23:03 +0600

    Обновила

--- Lesson-13/Problem-5/main.cpp
+++ Lesson-13/Problem-5/main.cpp
@@ -5,19 +5,11 @@
 int main() {
     double x, y;
     scanf("%lf%lf", &x, &y);
-    double temp_1 = x, temp_2 = 0;
-    int count = 0;
-    for(int i = 0; y >= 1; ++i) {
-        y -= temp_1;
-        printf("y = %f\n", y);
-        temp_1 = 0.7 * (x + temp_2);
-        printf("t1 = %f\n", temp_1);
-        temp_2 += temp_1;
-        printf("t2 = %f\n", temp_2);
+    int count = 1;
+    while(y - x > 10e-8) {
+        x *= 1.7;
         ++count;
-        printf("count = %d\n", count);
     }
-
     printf("%d", count);
     return 0;
 }
