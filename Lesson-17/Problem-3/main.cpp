cbc3a4204cb0cfca2e89433689431c3eaf7d0ec8
sh-alanova <38557996+sh-alanova@users.noreply.github.com>
2019-07-30 10:37:09 +0600

    Не знаю что писать в else

---
+++ Lesson-17/Problem-3/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+bool IsAlpha(char c) {
+    if((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z')) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+int main() {
+    string s;
+    string tmp, longest_word;
+    getline(cin, s);
+    for(int i = 0; i < s.size(); ++i){
+        if(IsAlpha(s[i])) {
+            tmp += s[i];
+        }
+        if(tmp.size() > longest_word.size()) {
+            longest_word = tmp;
+        }
+    }
+    cout << longest_word;
+}
+
+
+
+
+
